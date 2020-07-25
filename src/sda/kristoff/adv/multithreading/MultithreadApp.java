00c77a1bb97aea18e160d52b81a37ed004dcd9c4
Krzysztof Syska <prezydium834@gmail.com>
2020-07-25 13:38:45 +0200

    First Threads

--- src/sda/kristoff/adv/multithreading/MultithreadApp.java
+++ src/sda/kristoff/adv/multithreading/MultithreadApp.java
@@ -4,23 +4,34 @@ import java.math.BigDecimal;
 
 public class MultithreadApp {
 
-    public static void main(String[] args) {
-/*        Thread threadOne = new Thread();
-        threadOne.start();
-        */
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread threadOne = new Thread(() -> factorialWorker());
+        Thread threadTwo = new Thread(() -> factorialWorker());
+        Thread threadThree = new Thread(() -> factorialWorker());
+        Thread threadFour = new Thread(() -> factorialWorker());
+
+
         long start = System.currentTimeMillis();
-        factorialWorker();
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
+        threadFour.start();
+
+        threadOne.join();
+        threadTwo.join();
+        threadThree.join();
+        threadFour.join();
+
         System.out.println("Finished in: " + (System.currentTimeMillis() - start) + " ms");
     }
 
 
     private static void factorialWorker() {
-
         BigDecimal result = new BigDecimal(1);
         for (int i = 1; i < 5000; i++) {
             result = result.multiply(new BigDecimal(i));
             System.out.println(result);
         }
-
     }
 }
